7471b4e26b8c208ddcb4bda69538dbf1ea9b85fc
Alexander Grygoriev <truedevelop@yahoo.com>
2023-09-03 04:16:36 +0300

    start Solar Project clean

---
+++ js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('hello from solar');
+});
+//# sourceMappingURL=main.js.map
---
+++ js/main.ts
@@ -0,0 +1,7 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  console.log('hello from solar');
+});
