34cdf8fb05ae3d5db1a5ecd1c11f4b2fb5125733
Alexander Grygoriev <truedevelop@yahoo.com>
2023-09-03 05:14:05 +0300

    project DONE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello from solar');
+    let range = document.querySelector('.range');
+    let divider = document.querySelector('body');
+    range.addEventListener('input', (e) => {
+        const target = e.target;
+        const absCountWithoutMinus = Math.abs(+target.value);
+        divider.style.setProperty('--divider', '' + absCountWithoutMinus);
+        console.log(divider.style.getPropertyValue('--divider'));
+    });
 });
 //# sourceMappingURL=main.js.map
--- js/main.ts
+++ js/main.ts
@@ -1,7 +1,12 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
-  console.log('hello from solar');
+  let range = document.querySelector('.range') as HTMLInputElement;
+  let divider = document.querySelector('body') as HTMLBodyElement;
+  range.addEventListener('input', (e) => {
+    const target = e.target as HTMLInputElement;
+    const absCountWithoutMinus: number | string = Math.abs(+target.value);
+    divider.style.setProperty('--divider', '' + absCountWithoutMinus);
+    console.log(divider.style.getPropertyValue('--divider'));
+  });
 });
